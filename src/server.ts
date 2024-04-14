6fc6d5bad9b4f7f005a06334ebc37213a6a962c2
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2024-04-14 07:57:25 -0300

    publicFolder

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express'
+import path from 'path'
 import routes from './routes/index'
 import routerAdm from './adm'
 
 const server = express()
+console.log(path.join());
 
+
+server.use('/static', express.static(path.join(__dirname, '../public')))
 server.use(routes)
 server.use(routerAdm)
 server.use((req: Request, res: Response) => {
