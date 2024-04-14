4e451a5221c514a905ce4155bd497b7a5dee2510
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2024-04-14 12:55:18 -0300

    MustacheLoop

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express'
 const rounter = Router()
 
 rounter.get('/', (req: Request, res: Response) => {
-  let age: number = 90
+  let age: number = 28
   let showOld: boolean = false
   if (age > 50) {
     showOld = true
@@ -10,7 +10,17 @@ rounter.get('/', (req: Request, res: Response) => {
   res.render('home', {
     name: 'testador',
     lastName: 'master',
-    showOld
+    showOld,
+    products: [
+      { title: 'ovo', price: 10 },
+      { title: 'leite', price: 24 },
+      { title: 'nescau', price: 12 }
+    ],
+    dayPhrase: [
+      'qwertyuiop',
+      'asdfghjklç',
+      'zxcvbnm',
+    ]
   })
 })
 rounter.get('/contact', (req: Request, res: Response) => {
