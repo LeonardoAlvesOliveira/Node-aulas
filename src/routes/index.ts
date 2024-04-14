191204768a24f7d7c420693e563f00360ac06170
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2024-04-14 12:42:26 -0300

    mustachecond

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,15 @@ import { Router, Request, Response } from 'express'
 const rounter = Router()
 
 rounter.get('/', (req: Request, res: Response) => {
-
+  let age: number = 90
+  let showOld: boolean = false
+  if (age > 50) {
+    showOld = true
+  }
   res.render('home', {
     name: 'testador',
-    lastname: 'master'
+    lastName: 'master',
+    showOld
   })
 })
 rounter.get('/contact', (req: Request, res: Response) => {
